817cee78a3d7bcbb4dbee50e66ed4c65c58980a1
satpalslabs <126866915+satpalslabs@users.noreply.github.com>
2024-09-17 18:16:30 +0530

    build

--- src/components/home/core-services/containers/service-card.tsx
+++ src/components/home/core-services/containers/service-card.tsx
@@ -18,7 +18,7 @@ const ServiceCard = ({ item }: { item: dataItem }) => {
       ref={ref}
       className={`group mt-[73px] p-4 pb-8 sm:p-[9px] sm:pb-[14px] lg:p-[14px] lg:pb-[23px] relative shadow-buttonInset bg-innerContainer rounded-[32px] sm:rounded-[20px] flex flex-col transition-all duration-1000 hover:bg-gradientOnHover border-[1.5px] cursor-pointer hover:border-borderPrimary ${
         isInView
-          ? "border-viewportBorder  grayscale-0"
+          ? "border-viewportBorder  !grayscale-0"
           : "border-transparent grayscale"
       } `}
     >
