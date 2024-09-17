8f4b57e98491d251afd5eebc5bb7fe48e44b8af9
satpalslabs <126866915+satpalslabs@users.noreply.github.com>
2024-09-17 18:12:16 +0530

    build

--- src/components/home/core-services/index.tsx
+++ src/components/home/core-services/index.tsx
@@ -14,18 +14,16 @@ export type dataItem = {
   description: string;
 };
 const CoreServices: React.FC = () => {
-  const ref = useRef<HTMLDivElement>(null);
-  const isInView = useInView(ref);
 
   return (
     <div
       className={`px-[80px] lg:px-10 xs:px-5 py-[72px] pb-0 sm:pb-8 sm:pt-0 xs:py-0 bg-black max-w-[1920px] mx-auto`}
       id="core_services"
     >
-      <div ref={ref} className="grid grid-cols-3 gap-5 lg:gap-[18px] xxl:gap-16 xs:grid-cols-1 xs:gap-[28px]">
+      <div  className="grid grid-cols-3 gap-5 lg:gap-[18px] xxl:gap-16 xs:grid-cols-1 xs:gap-[28px]">
         {data &&
           data.map((item: dataItem, index: number) => {
-            return <ServiceCard item={item} isInView={isInView} key={index} />;
+            return <ServiceCard item={item} key={index} />;
           })}
       </div>
     </div>
