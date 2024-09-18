61d09b7d55878cb51d3685babfa9d52d0e954645
satpalslabs <126866915+satpalslabs@users.noreply.github.com>
2024-09-18 10:12:41 +0530

    Testing the whole website on safari

--- src/components/home/core-services/containers/service-card.tsx
+++ src/components/home/core-services/containers/service-card.tsx
@@ -5,29 +5,25 @@
 import dynamic from "next/dynamic";
 const ImageContainer = dynamic(() => import("./image-container"));
 const TextContainer = dynamic(() => import("./text-container"));
+import Card from "@components/basic-components/card";
 
 import { dataItem } from "..";
-import { useRef } from "react";
-import { useInView } from "@lib/use-in-view";
-const ServiceCard = ({ item }: { item: dataItem }) => {
-  const ref = useRef<HTMLDivElement>(null);
-  const isInView = useInView(ref);
 
-  return (
-    <div
-      ref={ref}
-      className={`group mt-[73px] p-4 pb-8 sm:p-[9px] sm:pb-[14px] lg:p-[14px] lg:pb-[23px] relative shadow-buttonInset bg-innerContainer rounded-[32px] sm:rounded-[20px] flex flex-col transition-all duration-1000 hover:bg-gradientOnHover border-[1.5px] cursor-pointer hover:border-borderPrimary ${
-        isInView
-          ? "border-viewportBorder "
-          : "border-transparent "
-      } `}
-    >
-      <div className="xxl:-mt-[15%] xl:-mt-[25%] lg:-mt-[20%] md:-mt-[15%] sm:-mt-[15%] xs:-mt-[4%] xs:-translate-y-[18%] w-full h-[150px] xl:h-[200px] lg:[100px] md:[83px] sm:h-[78px]  2xs:h-[60px]">
-        <ImageContainer imageId={item.id} />
-      </div>
-      <TextContainer item={item} />
+const ServiceCard = ({
+  isInView,
+  item,
+}: {
+  isInView: boolean;
+  item: dataItem;
+}) => (
+  <Card isInView={isInView}
+    style="mt-[73px] "
+  >
+    <div className="xxl:-mt-[15%] xl:-mt-[25%] lg:-mt-[20%] md:-mt-[15%] sm:-mt-[15%] xs:-mt-[4%] xs:-translate-y-[18%] w-full h-[150px] xl:h-[200px] lg:[100px] md:[83px] sm:h-[78px]  2xs:h-[60px]">
+      <ImageContainer imageId={item.id} />
     </div>
-  );
-};
+    <TextContainer item={item} />
+  </Card>
+);
 
 export default ServiceCard;
