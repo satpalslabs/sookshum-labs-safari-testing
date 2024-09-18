d6e663e12e4fa6341dee5ef63619ba3f541b7f17
satpalslabs <126866915+satpalslabs@users.noreply.github.com>
2024-09-18 18:26:55 +0530

    Testing the whole website on safari

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,35 @@
 const nextConfig = {
   // Other Next.js configuration options
   //  output:"export",
+  async headers() {
+    const headers = [];
+
+    if (process.env.VERCEL_ENV !== 'production') {
+      // For development and preview environments
+      headers.push({
+        source: "/(.*)", // Apply to all routes
+        headers: [
+          {
+            key: "X-Robots-Tag",
+            value: "noindex, nofollow", // Disallow crawling
+          },
+        ],
+      });
+    } else {
+      // Optionally, set "index, follow" explicitly for production
+      headers.push({
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-Robots-Tag",
+            value: "index, follow", // Allow crawling
+          },
+        ],
+      });
+    }
+
+    return headers;
+  },
 };
 
 export default nextConfig;
