1da794d4f2e71b612895ae28e59a3959f6c18ad6
satpalslabs <126866915+satpalslabs@users.noreply.github.com>
2024-09-18 09:47:00 +0530

    Testing the whole website on safari

--- src/components/home/core-services/containers/service-card.tsx
+++ src/components/home/core-services/containers/service-card.tsx
@@ -18,8 +18,8 @@ const ServiceCard = ({ item }: { item: dataItem }) => {
       ref={ref}
       className={`group mt-[73px] p-4 pb-8 sm:p-[9px] sm:pb-[14px] lg:p-[14px] lg:pb-[23px] relative shadow-buttonInset bg-innerContainer rounded-[32px] sm:rounded-[20px] flex flex-col transition-all duration-1000 hover:bg-gradientOnHover border-[1.5px] cursor-pointer hover:border-borderPrimary ${
         isInView
-          ? "border-viewportBorder  !grayscale-0"
-          : "border-transparent grayscale"
+          ? "border-viewportBorder "
+          : "border-transparent "
       } `}
     >
       <div className="xxl:-mt-[15%] xl:-mt-[25%] lg:-mt-[20%] md:-mt-[15%] sm:-mt-[15%] xs:-mt-[4%] xs:-translate-y-[18%] w-full h-[150px] xl:h-[200px] lg:[100px] md:[83px] sm:h-[78px]  2xs:h-[60px]">
