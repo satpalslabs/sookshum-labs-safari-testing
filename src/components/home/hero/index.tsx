da559709c5bf97ffcaf9683c5dbe08638a67e2d0
satpalslabs <126866915+satpalslabs@users.noreply.github.com>
2024-09-24 13:16:58 +0530

    Core -services animation issue. and performance issue

--- src/components/home/hero/index.tsx
+++ src/components/home/hero/index.tsx
@@ -16,13 +16,13 @@ const Hero: React.FC = () => (
     className="flex relative justify-between h-fit max-w-[1920px] mx-auto"
     id="hero"
   >
-    <div className="absolute  -top-[15%]  h-[200px] lg:-top-[10%] xs:top-[-15.4%] xs:z-[99999] w-screen overflow-hidden">
+    {/* <div className="absolute  -top-[15%]  h-[200px] lg:-top-[10%] xs:top-[-15.4%] xs:z-[99999] w-screen overflow-hidden">
       <img
         alt=""
         src="/home/hero/dots.svg"
         className="scale-[3.8] lg:scale-[3] xs:scale-[2] min-w-[100vw]  h-[100px]"
       />
-    </div>
+    </div> */}
     <TextContainer />
     <ImageContainer />
     <div className="absolute -bottom-[20%] lg:-bottom-[23%] xs:bottom-[8%] h-[200px] xs:h-[40px] z-20 w-full overflow-visible">
