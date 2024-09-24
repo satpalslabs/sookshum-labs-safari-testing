e407409676eeb7326fed15518c2074d02551932e
satpalslabs <126866915+satpalslabs@users.noreply.github.com>
2024-09-24 13:30:54 +0530

    Core -services animation issue. and performance issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,8 @@ export const metadata: Metadata = {
     icon: "/favicon.png",
   },
   title: "Sookshum Labs: Custom Software Development Company",
-  description:" We build modern digital products for startups and enterprises around the globe."
+  description:
+    " We build modern digital products for startups and enterprises around the globe.",
 };
 
 export default function RootLayout({
@@ -33,15 +34,15 @@ export default function RootLayout({
         />
         <link rel="stylesheet" />
         {/* Preconnect to optimize font loading */}
-        <link
-          rel="preconnect"
-          href="https://fonts.googleapis.com"
-          crossOrigin="anonymous"
-        />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
           href="https://fonts.gstatic.com"
           crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+          rel="stylesheet"
         />
       </Head>
       <body>
