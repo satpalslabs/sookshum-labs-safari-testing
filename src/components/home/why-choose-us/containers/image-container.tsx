e7642dd1c924979f8dd4aeba941f867ed8b721f7
satpalslabs <126866915+satpalslabs@users.noreply.github.com>
2024-09-25 16:49:43 +0530

    added core services again

--- src/components/home/why-choose-us/containers/image-container.tsx
+++ src/components/home/why-choose-us/containers/image-container.tsx
@@ -4,6 +4,7 @@
 
 import Image from "next/image";
 import { dataItemType } from "./main-container";
+import { useEffect, useState } from "react";
 
 const ImageContainer = ({
   item,
@@ -11,27 +12,47 @@ const ImageContainer = ({
 }: {
   item: dataItemType;
   isInView: boolean;
-}) => (
-  <div
-    className={`grow-0 flex items-center col-span-6
-       shadow-buttonInset bg-whyUsContainer rounded-[32px] lg:rounded-[26px] sm:gap-3 xs:hidden grayscale transition-all ${
-         isInView ? "grayscale-0" : "grayscale"
-       } }`}
-  >
-    <video
-      autoPlay={true}
-      src={item.video_url}
-      muted
-      loop
-      playsInline
-      className="w-[500px] m-auto  md:w-[368px] md:h-[368px] object-contain"
+}) => {
+  const [isRetina, setIsRetina] = useState(window.devicePixelRatio > 1);
+
+  useEffect(() => {
+    const checkDevicePixelRatio = () => {
+      setIsRetina(window.devicePixelRatio > 1);
+    };
+
+    // Initial check on mount
+    checkDevicePixelRatio();
+
+    // Add event listener for resize
+    window.addEventListener("resize", checkDevicePixelRatio);
+
+    return () => window.removeEventListener("resize", checkDevicePixelRatio);
+  }, []);
+
+  return (
+    <div
+      className={`grow-0 flex items-center col-span-6
+       shadow-buttonInset ${
+         isRetina ? "bg-whyUsContainer" : "bg-innerContainer"
+       } rounded-[32px] lg:rounded-[26px] sm:gap-3 xs:hidden grayscale transition-all ${
+        isInView ? "grayscale-0" : "grayscale"
+      } }`}
     >
-      <img
-        src={item.image_url}
-        title="Your browser does not support the <video> tag"
-      />
-    </video>
-  </div>
-);
+      <video
+        autoPlay={true}
+        src={item.video_url}
+        muted
+        loop
+        playsInline
+        className="w-[500px] m-auto  md:w-[368px] md:h-[368px] object-contain"
+      >
+        <img
+          src={item.image_url}
+          title="Your browser does not support the <video> tag"
+        />
+      </video>
+    </div>
+  );
+};
 
 export default ImageContainer;
