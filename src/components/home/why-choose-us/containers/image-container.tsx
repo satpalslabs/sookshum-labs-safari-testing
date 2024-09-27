20abf47e623ed1aa6a1b6e3961fe4e83432d4dbc
satpalslabs <126866915+satpalslabs@users.noreply.github.com>
2024-09-27 13:19:01 +0530

    final build testing

--- src/components/home/why-choose-us/containers/image-container.tsx
+++ src/components/home/why-choose-us/containers/image-container.tsx
@@ -43,15 +43,11 @@ const ImageContainer = ({
     }
 
     function isIpadOS() {
-      if (
+      return (
         navigator.maxTouchPoints &&
         navigator.maxTouchPoints > 2 &&
         /MacIntel/.test(navigator.platform)
-      ) {
-        return true;
-      } else {
-        return isIOS();
-      }
+      );
     }
   }, []);
 
