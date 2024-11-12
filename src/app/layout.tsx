71f98bc6f08e38fb5585ad7f4e8fc7b3387223d9
satpalslabs <126866915+satpalslabs@users.noreply.github.com>
2024-11-12 13:25:26 +0530

    updated the all background images

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,11 @@ import { GoogleAnalytics } from "@next/third-parties/google";
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://www.sookshum-labs.com"),
+  icons: {
+    icon: ["/favicon.svg", "/favicon.png"],
+    apple: "/favicons/apple-touch-icon.png",
+    shortcut: ["/favicon.svg", "/favicon-96x96.png"],
+  },
   alternates: {
     canonical: "https://www.sookshum-labs.com/",
     languages: {
