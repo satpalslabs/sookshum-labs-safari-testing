96ba1d316c20db7766b6924ef2adc5930834ee8b
satpalslabs <126866915+satpalslabs@users.noreply.github.com>
2024-12-04 12:11:18 +0530

    issue-Colors of video doesn't matched

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -8,7 +8,7 @@ import GoTopButton from "@components/basic-components/go-to-top";
 // import Loading from "@app/loading";
 
 const Hero = dynamic(() => import("./hero"));
-// const CoreServices = dynamic(() => import("./core-services"));
+const CoreServices = dynamic(() => import("./core-services"));
 const OurServices = dynamic(() => import("./our-services"));
 const DevelopmentProcess = dynamic(() => import("./our-development-process"));
 const ChooseUs = dynamic(() => import("./why-choose-us"));
@@ -22,7 +22,7 @@ const WorkTogether = dynamic(() => import("../basic-components/work-together"));
 const Sections: React.FC<HomeProps> = (props) => (
   <main className="mt-14 xl:mt-[52px] bg-black lg:mt-0 h-full">
     <Hero />
-    {/* <CoreServices /> */}
+    <CoreServices />
     <OurServices />
     <DevelopmentProcess />
     <ChooseUs />
