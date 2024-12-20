b9c6698146f464bc0fcceb068d8e2491520594ed
satpalslabs <126866915+satpalslabs@users.noreply.github.com>
2024-12-20 17:33:44 +0530

    SL6-T206
             -  Completed the responsive design for the following pages:              - Headless CMS Development services              - Strapi CMS Development Service

--- src/components/contact-us/hero.tsx
+++ src/components/contact-us/hero.tsx
@@ -6,7 +6,7 @@ const Hero: React.FC = () => {
       <div className="w-full relative z-20 flex flex-col gap-3 xs:gap-0 lg:mt-[71px] sm:mt-[23px] xs:mt-[30px] h-fit overflow-hidden px-[80px] lg:px-10 xs:px-5 pt-[72px] lg:pt-[0px] xs:pb-0 max-w-[1920px] mx-auto">
         <GradientText
           text={"Contact us"}
-          classes={`sm:text-[48px] w-fit xs:text-[32px] text-left xs:leading-[1.3]`}
+          classes={`sm:text-[48px] w-fit xs:text-[32px] text-left xs:leading-[1.3] hero-span`}
         />
         <H3
           style="xs:text-[12px] !leading-[1.3]"
